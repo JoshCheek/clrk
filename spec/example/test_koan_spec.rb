72e37727403b97db8e73f30b06d313871462f5c9
Josh Cheek <josh.cheek@gmail.com>
2012-03-04 01:29:28 -0600

    Forget stupid "look in caller history to find the right dir", just initialize an object w/ right dir

--- spec/example/test_koan_spec.rb
+++ spec/example/test_koan_spec.rb
@@ -1,33 +1,27 @@
 require 'spec_helper'
 
-RSpec.configure do |config|
-  config.include TestKoan
-end
-
 describe 'the test_koan rspec helper' do
-  before do
-    TestKoan.execute_script = Proc.new do |script|
-      @last_script = script
-    end
-  end
-
   def script
     @last_script
   end
 
+  let :runner do
+    CLRK::TestKoan.new(File.dirname __FILE__) { |script| @last_script = script }
+  end
+
   context 'takes the :stdin option' do
     specify "and sets the input file to the script's stdin" do
-      test_koan :stdin => "input1.txt", :result => "output1.txt"
+      runner.test_koan :stdin => "input1.txt", :result => "output1.txt"
       script.index("cat input1.txt |").should == 0
     end
   end
 
   context 'takes the :argv option' do
     specify "and sets the filenames as the script's arguments" do
-      test_koan :argv => "input1.txt", :result => "output1.txt"
+      runner.test_koan :argv => "input1.txt", :result => "output1.txt"
       script.end_with?(" input1.txt").should be_true
 
-      test_koan :argv => %w[input1.txt input2.txt], :result => "output1.txt"
+      runner.test_koan :argv => %w[input1.txt input2.txt], :result => "output1.txt"
       script.end_with?(" input1.txt input2.txt").should be_true
     end
   end
